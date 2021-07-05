3c50ea9421daeee41665473b00a801ee311482fd
Hithman_01 <Mayronmartins013@gmail.com>
2021-07-05 19:22:28 -0300

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/Controller/Principal.java
+++ src/Controller/Principal.java
@@ -5,10 +5,45 @@
  */
 package Controller;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mayro
  */
 public class Principal {
+    private ArrayList <Integer> array = new ArrayList<>();
+    private ArrayList <Integer> arrayOrdenado = new ArrayList<>();
     
+    public void limparTabelas(){
+        //Pega as tabelas e limpa os campos
+    }
+    public void gerarNumeros(){
+        //Pega a quantidade de números a seres gerados
+        //Inicia a função de geração de números com retorno do array
+        //Joga o valor da função em array
+    }
+    
+    public void importarNumeros(){
+        //Inicia a função de importação de valores
+        //Joga o valor em array
+        //Implementa a função Thread de Leitura de cada método de seleção
+    }
+    
+    public void implementarOrdenacao(){
+        //limpar tabelas
+        //Verifica e pega o valor do array
+        Quick_Sort quickSorte = new Quick_Sort();
+        Selection_Sort selectionSort = new Selection_Sort();
+        
+        for(int repet=0; repet<50; repet++){
+            //Realiza a operação de ordenação com o Selection
+            //Coloca o valor de tempo no campo correspondente
+            
+            //Realiza a operação de ordenação com Quick
+            //Coloca o valor de tempo no campo correspondente
+        }
+        
+        //Inicia a função de exportação do array para pasta designada
+    }
 }

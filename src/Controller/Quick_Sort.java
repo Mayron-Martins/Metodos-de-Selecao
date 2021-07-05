c20b138c57a0789adbd73cc1c9f337ceebaa5588
Hithman_01 <Mayronmartins013@gmail.com>
2021-07-05 18:19:35 -0300

    Second

--- src/Controller/Quick_Sort.java
+++ src/Controller/Quick_Sort.java
@@ -5,10 +5,50 @@
  */
 package Controller;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mayro
  */
 public class Quick_Sort {
+    private long timeQuick = 0;
     
+    public ArrayList<Integer> QuickSort(ArrayList<Integer> array,  int inicio, int fim){
+        timeQuick = System.nanoTime();
+        if (inicio < fim) {
+            int posicaoPivo = separar(array, inicio, fim);
+            QuickSort(array, inicio, posicaoPivo - 1);
+            QuickSort(array, posicaoPivo + 1, fim);
+        }
+        timeQuick = System.nanoTime()-timeQuick;
+        return array;
+    }
+    
+       private int separar(ArrayList<Integer> vetor, int inicio, int fim) {
+             int pivo = vetor.get(inicio);
+             int i = inicio + 1, f = fim;
+             while (i <= f) {
+                    if (vetor.get(i) <= pivo)
+                           i++;
+                    else if (pivo < vetor.get(f))
+                           f--;
+                    else {
+                           int troca = vetor.get(i);
+                           vetor.set(i, vetor.get(f));
+                           vetor.set(f, troca);
+                           i++;
+                           f--;
+                    }
+             }
+             vetor.set(inicio, vetor.get(f));
+             vetor.set(f, pivo);
+             return f;
+       }
+
+    public long getTimeQuick() {
+        return timeQuick;
+    }
+       
+       
 }
